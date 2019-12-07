177bfafc51795201e18c981a2eeed6a484a97496
zyh <zhang.yonghong@chinaebi.com>
2019-12-07 12:11:34 +0800

    add public

---
+++ public/admin.php
@@ -0,0 +1,28 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2018 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// [ 应用入口文件 ]
+namespace think;
+
+if (!defined('__PUBLIC__')) {
+    $_public = rtrim(dirname(rtrim($_SERVER['SCRIPT_NAME'], '/')), '/');
+    define('__PUBLIC__', (('/' == $_public || '\\' == $_public) ? '' : $_public));
+}
+
+define('__GROUP__', $_public . '/admin');
+
+// 加载基础文件
+require __DIR__ . '/../thinkphp/base.php';
+
+// 支持事先使用静态方法设置Request对象和Config对象
+
+// 执行应用并响应
+Container::get('app')->bind('admin')->run()->send();
