1685bee0ead6fed1d63d06b27bf1d96b43a77c51
jikeytang <jikeytang@gmail.com>
2021-04-09 08:49:34 +0800

    update dbpwd

--- config/database.php
+++ config/database.php
@@ -15,7 +15,8 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'jing_ui_com',
+//    'database'        => 'jing_ui_com',
+    'database'        => 'jing-ui',
     // 用户名
     'username'        => 'root',
     // 密码
